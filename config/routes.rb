4d82dff4343363eac88461da47e18526cd0d0c9a
Juan Leonel <vazquez.juan.1fm@gmail.com>
2021-07-10 22:38:58 -0500

    NANI4

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,17 @@ Rails.application.routes.draw do
 
 ##ROOT PATH: 
   root to: "home#index"
-  resources :articles do 
-    get "user/:user_id", to: "articles#from_author", on: :colection
-  end
 
-  # get "articles", to: "articles#index"
-  # get "articles/new", to: "articles#new", as: :new_articles
-  # get "articles/:id", to: "articles#show"
-  # get "articles/:id/edit", to: "articles#edit"
-  # patch "articles/:id", to: "articles#update", as: :article
-  # post "articles", to: "articles#create"
-  # delete "articles/:id", to: "articles#destroy"
+  get "articles/user/:user_id", to: "articles#from_author"
+
+  
+
+  get "articles", to: "articles#index"
+  get "articles/new", to: "articles#new", as: :new_articles
+  get "articles/:id", to: "articles#show"
+  get "articles/:id/edit", to: "articles#edit"
+  patch "articles/:id", to: "articles#update", as: :article
+  post "articles", to: "articles#create"
+  delete "articles/:id", to: "articles#destroy"
+
 end
