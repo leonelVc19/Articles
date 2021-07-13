c6278766f8bb9ca4670f9e1008dc782dec91738a
Juan Leonel <vazquez.juan.1fm@gmail.com>
2021-07-12 21:31:20 -0500

    Resources Activado

--- config/routes.rb
+++ config/routes.rb
@@ -7,16 +7,16 @@ Rails.application.routes.draw do
 ##ROOT PATH: 
   root to: "home#index"
 
-  get "articles/user/:user_id", to: "articles#from_author"
 
-  
-
-  get "articles", to: "articles#index"
-  get "articles/new", to: "articles#new", as: :new_articles
-  get "articles/:id", to: "articles#show"
-  get "articles/:id/edit", to: "articles#edit"
-  patch "articles/:id", to: "articles#update", as: :article
-  post "articles", to: "articles#create"
-  delete "articles/:id", to: "articles#destroy"
+  resources :articles do
+    get "user/:user_id", to: "articles#from_author", on: :collection
+  end
+  #get "articles", to: "articles#index"
+  #get "articles/new", to: "articles#new", as: :new_articles
+  #get "articles/:id", to: "articles#show"
+  #get "articles/:id/edit", to: "articles#edit"
+  #patch "articles/:id", to: "articles#update", as: :article
+  #post "articles", to: "articles#create"
+  #delete "articles/:id", to: "articles#destroy"
 
 end
