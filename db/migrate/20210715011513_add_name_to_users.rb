b699863b592c69b24583fe7dadc339bbb45d754f
Juan Leonel <vazquez.juan.1fm@gmail.com>
2021-07-30 22:20:07 -0500

    INICIO NUEVO

--- db/migrate/20210715011513_add_name_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddNameToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :paternal_surname, :string
-    add_column :users, :maternal_surname, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_011513) do
+ActiveRecord::Schema.define(version: 2021_07_29_180732) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -84,9 +84,8 @@ ActiveRecord::Schema.define(version: 2021_07_15_011513) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.string "paternal_surname"
-    t.string "maternal_surname"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
