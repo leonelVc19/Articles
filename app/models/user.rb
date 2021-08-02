1febde2ca08b3fc31c4ce6b032ec7c5282c0900a
Juan Leonel <vazquez.juan.1fm@gmail.com>
2021-08-01 19:35:50 -0500

    Cambios guardados

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many :articles, dependent: :nullify
+         has_many :articles
+         has_many :requests
          has_one_attached :avatar
-
-  has_many :requests
 end
